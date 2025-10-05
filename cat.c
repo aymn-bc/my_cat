6f33d0613c2b6b1c024e2f5ad4c25bb24e4fad63
aymen-bc <aymenbencheikh789@gmail.com>
2025-10-05 13:25:07 +0100

    fix Segmentation fault

--- cat.c
+++ cat.c
@@ -13,34 +13,44 @@ int main(int argc, char **argv){
     int ch;
     char **error = NULL;
     if (argc == 1){
-        theFile[argc-1] = stdin;  
+        theFile[argc-1] = stdin;
         while((ch = getc(theFile[argc - 1])) != EOF){
             putc(ch, stdout);
         }  
     }
     else if ( argc >= 2 ){
+        error = (char**) malloc ((argc - 1) * sizeof(char*));
         for (int i = 1; i < argc; i++){
-            theFile[i] = fopen(argv[i], "r");
             if (!isFile(argv[i])){
                 size_t len = strlen("Error: \"\" is a Directory!\n") + strlen(argv[i]) + 1;
                 error[j] = (char*) malloc(len);
-                if ( error[j] != NULL ) snprintf(error, sizeof(error), "Error: \"%s\" is a Directory!\n", argv[i]);
+                if ( error[j] != NULL ) snprintf(error[j], len, "Error: \"%s\" is a Directory!\n", argv[i]);
                 j++;
                 continue;
             }
-            else if (errno == ENOENT) {
+            else if (isFile(argv[i]) == -1) {
                 size_t len = strlen("Error: No such file or directory named \"\" is not found!\n") + strlen(argv[i]) + 1;
                 error[j] = (char*) malloc(len);
-                if ( error[j] != NULL ) snprintf(error, sizeof(error), "Error: No such file or directory named \"%s\" is not found!\n", argv[i]);
+                if ( error[j] != NULL ) snprintf(error[j], len, "Error: No such file or directory named \"%s\" is not found!\n", argv[i]);
                 j++;
                 continue;
             }
-            
-            while ((ch = getc(theFile[i])) != EOF){
-                putc(ch, stdout);
+            else{
+                theFile[i] = fopen(argv[i], "r");
+                while ((ch = getc(theFile[i])) != EOF){
+                    putc(ch, stdout);
+                }
+                fclose(theFile[i]);
             }
-            fclose(theFile[i]);
-        }        
+        }
+        if (error != NULL){
+            for (int i = 0; i < j; i++){
+                printf("%s", error[i]);
+                free(error[i]);
+            }
+            free(error);
+        }
+              
     }
 }
     
@@ -49,9 +59,11 @@ int main(int argc, char **argv){
 int isFile(char *name){
     DIR *directory = opendir(name);
     if (directory != NULL) {
+        printf("\nWorkss\n");
         closedir(directory);
         return 0;
-    }else{
-        return 1;
+    }else if (errno == ENOENT){
+        return -1;
     }
+    return 1;
 }
